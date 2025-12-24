ae0c5044c7bab7e662f98391b76a3aa0858090e8
ayomideolaleye0987-star <ayomideolaleye0987@gmail.com>
2025-12-24 08:11:00 +0000

    tt

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,22 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ConnectKitProvider, getDefaultClient } from 'connectkit'
-import { configureChains, createClient, WagmiConfig, chain } from 'wagmi'
+import { configureChains, createConfig, WagmiConfig, chain } from 'wagmi'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 const avalanche = chain.avalanche
 
-const { chains, provider } = configureChains(
+const { chains, publicClient } = configureChains(
   [avalanche],
   [jsonRpcProvider({ rpc: () => ({ http: 'https://api.avax.network/ext/bc/C/rpc' }) })]
 )
 
-const { connectors } = getDefaultClient({ appName: 'AVAX CHRONICLES', chains })
-
-const wagmiClient = createClient({ autoConnect: true, connectors, provider })
+const wagmiConfig = createConfig(getDefaultClient({ appName: 'AVAX CHRONICLES', chains, publicClient }))
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <WagmiConfig client={wagmiClient}>
-      <ConnectKitProvider>
+    <WagmiConfig config={wagmiConfig}>
+      <ConnectKitProvider chains={chains}>
         <App />
       </ConnectKitProvider>
     </WagmiConfig>
